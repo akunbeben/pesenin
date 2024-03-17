af589d93c1215e4c0aa19dd3667b70e3b94214ff
Benny Rahmat <akunbeben@gmail.com>
2024-03-17 18:58:07 +0800

    cs

--- app/Providers/Filament/CentralPanelProvider.php
+++ app/Providers/Filament/CentralPanelProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers\Filament;
 
-use App\Filament\Central\Pages\Dashboard;
 use App\Filament\Central\Pages\Login;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
